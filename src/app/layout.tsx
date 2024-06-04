781ae6f4ddec6ff8b169bf6f9f3a137a4f66c997
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-04 17:34:06 +0300

    Add poppins font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-import Link from "next/link";
+import { ReactNode } from "react";
+import { Poppins } from "next/font/google";
 import { Providers } from "@/contexts/providers";
+
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const poppins = Poppins({
+  weight: ["400", "500"],
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>
-        <Providers>
-          <header>
-            <Link href="/">Home</Link>
-            <br />
-
-            <Link href="/profile">Profile</Link>
-          </header>
-          {children}
-        </Providers>
+      <body className={poppins.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
