126ed1cc71ab7315897ed933f006ef1cc85513cc
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-04 17:48:31 +0300

    Add tailwindcss configuration.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,22 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "neutral-1": "#fefefe",
+        "neutral-2": "#f3f5f7",
+        "neutral-3": "#e8ecef",
+        "neutral-4": "#6c7275",
+        "neutral-5": "#343839",
+        "neutral-6": "#232627",
+        "neutral-7": "#141718",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
