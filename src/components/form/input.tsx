8c6374bdf598bbb448fee44638c7b7f7e589c73d
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-06 00:21:21 +0300

    Update input component.

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,22 +1,63 @@
-import { forwardRef, InputHTMLAttributes } from "react";
+"use client";
+
+import { useState, useId, forwardRef, InputHTMLAttributes, HTMLInputTypeAttribute } from "react";
+import { Eye, EyeOff } from "lucide-react";
 import { cn } from "@/lib/utils/cn";
 
-export type InputProps = InputHTMLAttributes<HTMLInputElement>;
+export type InputProps = Omit<InputHTMLAttributes<HTMLInputElement>, "id" | "type"> & {
+  placeholder: string;
+  type: HTMLInputTypeAttribute;
+};
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, placeholder, type, ...props }, ref) => {
+    const isPasswordInput = type === "password";
+    const [isInputVisible, setIsInputVisible] = useState<boolean>(!isPasswordInput);
+    const passwordInputType = isInputVisible ? "text" : "password";
+
+    const id = useId();
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "placeholder:text-gray-40 bg-gray-10 3xl:py-6 flex h-10 w-full rounded-lg border px-5 py-4 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 lg:py-5",
-          className,
+      <div className="relative w-full">
+        <input
+          ref={ref}
+          id={id}
+          // placeholder has to exist (at least as space) to use the placeholder-shown pseudo class
+          // to simulate the placeholder behavior in the label element
+          placeholder=" "
+          type={isPasswordInput ? passwordInputType : type}
+          aria-label={placeholder}
+          className={cn(
+            "peer w-full border-b border-neutral-4 py-1 transition-colors bg-inherit focus:outline-none focus:border-neutral-7",
+            className
+          )}
+          {...props}
+        />
+        <label
+          htmlFor={id}
+          className={cn(
+            "absolute text-neutral-4 left-0 cursor-text text-xs -top-4 transition-all peer-focus:text-neutral-7 peer-focus:font-medium peer-placeholder-shown:top-1 peer-placeholder-shown:text-base"
+          )}
+        >
+          {placeholder}
+        </label>
+        {isPasswordInput && (
+          <button
+            type="button"
+            onClick={() => setIsInputVisible((prev) => !prev)}
+            aria-label={isInputVisible ? "Hide password" : "Show password"}
+            className="absolute right-0 top-1/2 -translate-y-1/2"
+          >
+            {isInputVisible ? (
+              <EyeOff color="#6c7275" size={18} />
+            ) : (
+              <Eye color="#6c7275" size={18} />
+            )}
+          </button>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     );
-  },
+  }
 );
 
 Input.displayName = "Input";
