eaff7f5dffc7e3113d8bb61c37a13e551992629d
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-06 16:04:36 +0300

    Add signup function in the auth services.

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,13 @@
 import { JWT } from "next-auth/jwt";
 import { axios } from "@/lib/axios";
-import { BackendTokens } from "@/types";
+import { BackendTokens, User } from "@/types";
+import { SignupSchema } from "@/lib/schemas/signup";
+
+export async function signup(signupData: Omit<SignupSchema, "agreeOnTerms">) {
+  const res = await axios.post<User>("/auth/register", signupData);
+  console.log(res);
+  return res.data;
+}
 
 export async function refreshToken(token: JWT): Promise<JWT> {
   const { data: tokens } = await axios.post<BackendTokens>("/auth/refresh", {
