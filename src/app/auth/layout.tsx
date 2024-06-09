9ac28149c6d5c1aeda9ec6b7e6b670929ca010e1
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-10 02:34:40 +0300

    Add link to the home page in the auth pages.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import { UserEmailContextProvider } from "@/contexts/user-email";
 import img from "@/assets/imgs/auth-page-background.png";
+import Link from "next/link";
 
 type LayoutProps = {
   children: ReactNode;
@@ -10,21 +11,23 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <section className="grid md:grid-cols-2">
-      <div className="relative h-full" aria-hidden="true">
+      <div className="relative h-full">
         <Image
           src={img}
           alt=""
           placeholder="blur"
           quality={100}
           priority={true}
+          aria-hidden="true"
           className="object-cover w-full h-[50svh] select-none md:min-h-svh"
         />
-        <span
-          className="text-black absolute top-5 left-1/2 -translate-x-1/2 font-medium text-3xl"
-          aria-label="Furniro"
+        <Link
+          href="/#"
+          className="text-neutral-7 absolute top-5 left-1/2 -translate-x-1/2 text-3xl font-semibold"
+          aria-label="Go back to home page"
         >
           Furniro
-        </span>
+        </Link>
       </div>
       <div className="container max-w-md px-6" role="main">
         <UserEmailContextProvider>{children}</UserEmailContextProvider>
