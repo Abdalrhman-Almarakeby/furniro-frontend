26f9972564a6f73027c2925b97332eb4b64a8a6a
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-12 20:03:34 +0300

    Fix an issue with category card link height.

--- src/app/(main-layout)/(home)/category-card-link.tsx
+++ src/app/(main-layout)/(home)/category-card-link.tsx
@@ -12,13 +12,13 @@ export function CategoryCardLink({ category, className, ...props }: CategoryCard
   return (
     <Link
       className={cn(
-        "absolute grid gap-2 bottom-6 left-6 sm:bottom-8 sm:left-8 lg:gap-3",
+        "absolute grid gap-2 bottom-6 left-6 sm:bottom-8 sm:left-8 lg:gap-3 h-fit",
         className
       )}
       {...props}
     >
       <span className="h6 lg:h5">{category}</span>
-      <span className="text-sm  items-center flex gap-1 font-medium border-b justify-self-start lg:text-base">
+      <span className="text-sm items-center flex gap-1 font-medium border-b justify-self-start lg:text-base">
         Shop now
         <ArrowRight className="size-4 lg:size-5" />
       </span>
